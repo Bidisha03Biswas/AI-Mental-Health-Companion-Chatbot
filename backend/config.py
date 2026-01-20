bb91477c2bf3e4d70c786fdad2cdd640be0d33a3
Bidisha Biswas <bidisha03biswas@gmail.com>
2026-01-20 12:02:53 +0530

    Update Twilio credentials and emergency contact

--- backend/config.py
+++ backend/config.py
@@ -1,6 +1,5 @@
-
-TWILIO_ACCOUNT_SID ="TWILIO_ACCOUNT_SID"
-TWILIO_AUTH_TOKEN ="TWILIO_AUTH_TOKEN"
-TWILIO_FROM_NUMBER = "TWILIO_NUMBER"   # Twilio-owned number
-EMERGENCY_CONTACT = "YOUR_COUNTRY_SAFETY_NUMBER"  # MUST include country code #Replace with actual emergency contact number
+TWILIO_ACCOUNT_SID ="ACa2a79a646dd59206cf421877226f6da"
+TWILIO_AUTH_TOKEN ="a641e1b538db3609840f07300231b90d5"
+TWILIO_FROM_NUMBER = "+12184966951"   # Twilio-owned number
+EMERGENCY_CONTACT = "+918918630941"  # MUST include country code #Replace with actual emergency contact number
 GROQ_API_KEY = "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"
